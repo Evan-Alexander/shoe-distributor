d7603b740e7382c08e50d302cd28739b23d65a2b
Jason Brown <jasontbrown99@gmail.com>
2017-03-03 16:57:56 -0800

    play around with delete brand from store

--- app/app.php
+++ app/app.php
@@ -48,6 +48,9 @@
         $brand->update($_POST['new-brand']);
         return $app['twig']->render('edit-brands.html.twig', array('brand' => $brand, 'stores' => $brand->getstores(), 'all_stores' => Store::getAll()));
     });
+         /* When trying to use redirect method here return $app->redirect('/brands/'. $id);
+         I get a notification that the page is written in Afrikaans and Google asks if the viewer
+         wants translation.  */
     $app->post("/update/{id}", function($id) use ($app) {
         $new_store = new Store($_POST['add-store']);
         $new_store->save();
@@ -55,12 +58,13 @@
         $brand->addStore($new_store);
         return $app['twig']->render('edit-brands.html.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
     });
+
 // STORES
     $app->get("/stores", function() use ($app) {
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
-    $app->post("/store", function() use ($app) {
+    $app->post("/stores", function() use ($app) {
         $store = new Store($_POST['store-name']);
         $store->save();
         return $app->redirect('/store');
@@ -82,6 +86,7 @@
         $store->update($_POST['new-store']);
         return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getbrands(), 'all_brands' => Brand::getAll()));
     });
+
     $app->post("/update-store/{id}", function($id) use ($app) {
         $new_brand = new Brand($_POST['add-brand']);
         $new_brand->save();
@@ -89,5 +94,12 @@
         $store->addBrand($new_brand);
         return $app->redirect('/stores/'. $id);
     });
+
+    $app->delete("/delete-brand-from-store/{id}", function($id) use ($app) {
+        $brand = Brand::find($id);
+        $brand->delete();
+        return $app['twig']->render('edit-stores.html.twig', array('brand' => Brand::find($id),'stores' => Store::getAll()));
+    });
+
     return $app;
 ?>
